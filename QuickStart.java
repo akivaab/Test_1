7aba9f46c136f701f58b3e59a0658638603d2e4f
akivaab <amabramo18@gmail.com>
2021-02-25 20:36:45 +0200

    enlightenment

--- QuickStart.java
+++ QuickStart.java
@@ -2,5 +2,7 @@ class QuickStart{
     public static void main(String[] args) {
         System.out.println("I DID IT!!!");
         System.out.println(":)");
+        int theAnswer = 42;
+        System.out.println("The answer to life, the universe and everything is " + theAnswer);
     }
 }
