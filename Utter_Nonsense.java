dcbab1bd5609230cb6df3290e301b38cf0c95ef2
akivaab <amabramo18@gmail.com>
2021-02-25 20:42:40 +0200

    dunno

---
+++ Utter_Nonsense.java
@@ -0,0 +1,7 @@
+class Nonsense{
+    private static String gibberish = "honwehfnoqxeg";
+    public static void main(String[] args){
+        System.out.println(gibberish);
+        System.out.println("Meaning has no meaning");
+    }
+}
