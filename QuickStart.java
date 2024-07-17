6aeb15b7c09aeee25810449d8a913d23db1ea247
akivaab <amabramo18@gmail.com>
2024-07-17 19:07:17 +0300

    new pc y'all

--- QuickStart.java
+++ QuickStart.java
@@ -4,5 +4,6 @@ class QuickStart{
         System.out.println(":)");
         int theAnswer = 42;
         System.out.println("The answer to life, the universe and everything is " + theAnswer);
+        System.out.println("The future calls from the beyond!");
     }
 }
